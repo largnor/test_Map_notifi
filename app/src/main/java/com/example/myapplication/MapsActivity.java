374173d6c26cf021c880454bfac168979d5e45d1
SJH <sjhblieve@naver.com>
2023-04-27 23:18:32 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -0,0 +1,239 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Toast;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.LocationSource;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.CircleOverlay;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class MapsActivity extends Activity implements OnMapReadyCallback,LocationSource.OnLocationChangedListener{
+    private static final int NOTIFICATION_PERMISSION_CODE = 123;
+    private MapView mapView;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private LocationSource.OnLocationChangedListener onLocationChangedListener;
+    private NaverMap naverMap;
+    private int GEOFENCE_RADIUS = 200;
+    private int aram_call = 0;
+    private Location mCurrentLocation;
+    private LatLng mOverlayLocation = new LatLng(35.267207,129.232717); // 임의의 오버레이 영역 좌표
+    private String location_name;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        Intent intent = getIntent();
+        mapView = findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+        locationSource = new FusedLocationSource(this,LOCATION_PERMISSION_REQUEST_CODE);
+
+        locationSource.activate((LocationSource.OnLocationChangedListener) this);
+        mapView.getMapAsync(this);
+
+
+        Bundle bundle = intent.getExtras();
+        location_name = bundle.getString("location_name");
+
+
+        // 알림 권한 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("my_channel_id", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+            if (checkSelfPermission(android.Manifest.permission.ACCESS_NOTIFICATION_POLICY) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_NOTIFICATION_POLICY}, NOTIFICATION_PERMISSION_CODE);
+            }
+        }
+
+    }
+
+
+
+
+
+    // request code와 권한획득 여부 확인
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (locationSource.onRequestPermissionsResult( requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+                return;
+            }
+        }
+
+        if (requestCode == NOTIFICATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "알림 권한이 승인되었습니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "알림 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+    }
+
+    // 맵을 구현하고 좌표에 맞는 영역 구현
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+            locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+            naverMap.setLocationSource(locationSource);
+
+
+            UiSettings uiSettings = naverMap.getUiSettings();
+            uiSettings.setLocationButtonEnabled(true);
+
+
+            //원 생성
+            CircleOverlay circleOverlay = new CircleOverlay();
+            circleOverlay.setCenter(mOverlayLocation);
+            circleOverlay.setRadius(GEOFENCE_RADIUS);
+            circleOverlay.setColor(Color.TRANSPARENT);
+            circleOverlay.setOutlineColor(Color.RED);
+            circleOverlay.setOutlineWidth(3);
+            CameraUpdate cameraUpdate = CameraUpdate.zoomTo(15); // 카메라 줌설정
+
+            naverMap.setLocationSource(locationSource);
+
+            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow); // 위치추적 follow 모드 활성 조작시 nofollow 모드로 바뀜
+
+            circleOverlay.setMap(naverMap);
+            naverMap.moveCamera(cameraUpdate);
+
+
+        }
+/// 위치값이 변경되는걸 감지하고 알림을 호출하는 부분
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+            int count=0;
+            mCurrentLocation = location;
+             Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+            if (onLocationChangedListener != null) {
+                onLocationChangedListener.onLocationChanged(location);
+            }
+
+            // 현재 위치와 오버레이 좌표간의 거리 계산
+            float[] distance = new float[2];
+            Location.distanceBetween(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(),
+                    mOverlayLocation.latitude, mOverlayLocation.longitude, distance);
+            float distanceInMeters = distance[0];
+
+            // 거리가 설정한 반경보다 작을 경우 알림 출력
+            if (distanceInMeters <= GEOFENCE_RADIUS) {
+                if (aram_call == 0) {
+                    createNotification();
+                    aram_call++;
+                }
+            }
+    }
+
+
+    public void activate(LocationSource.OnLocationChangedListener listener) {
+        onLocationChangedListener = listener;
+    }
+
+    public void deactivate() {
+        onLocationChangedListener = null;
+    }
+
+
+    //// 알림 채널 생성 및 호출
+    public void createNotification(){
+        show();
+    }
+    private void show(){
+        int notificationId = 1;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "alarm_test");
+        builder.setContentTitle("도착했습니다");
+        builder.setContentText("도착했습니다");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            AudioAttributes attributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build();
+            NotificationChannel channel = new NotificationChannel("alarm_test", "테스트채널", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION), attributes);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        builder.setLargeIcon(largeIcon);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        long[] pattern = {0, 500, 1000};
+        builder.setVibrate(pattern);
+
+        Uri ringtoneUri = RingtoneManager.getActualDefaultRingtoneUri(this, RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(ringtoneUri);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(notificationId, builder.build());
+   }
+
+    public void removeNotification(){
+        hide();
+    }
+    public void hide(){
+        NotificationManagerCompat.from(this).cancel(1);
+    }
+
+
+    // 뒤로가기 작동
+    @Override
+    public void onBackPressed() {
+       super.onBackPressed();
+       locationSource = null;
+       removeNotification();
+
+       finish();
+    }
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/textInput.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.Manifest;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class textInput extends AppCompatActivity {
+    private static final int NOTIFICATION_PERMISSION_CODE = 123;
+    EditText editText_address;
+    Button search_button;
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_text);
+
+
+        editText_address = findViewById(R.id.editText_address);
+        search_button = findViewById(R.id.search_button);
+
+
+
+        // 버튼 활성화
+        //주소이름이 textInput 클래스에서 MapsActivity 클래스로 이동
+        search_button.setClickable(true);
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = editText_address.getText().toString();
+
+
+                Intent intent = new Intent(textInput.this, MapsActivity.class);
+                intent.putExtra("location_name",location);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("종료");
+        builder.setMessage("종료하시겠습니까?");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 종료 버튼 클릭시 처리할 로직
+                finishAffinity(); // 앱 종료
+            }
+        });
+        builder.setNegativeButton("아니오", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
